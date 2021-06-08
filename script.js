151958c6a0cc830300a31654e1f35486e9bb5f9d
Guillaume BUISSON <guillaume.buisson.simplon@gmail.com>
2021-06-08 15:03:11 +0200

    Add class Player and playermovement

--- script.js
+++ script.js
@@ -30,18 +30,106 @@ box.style.border = "solid 2px #bbb44f";
 box.style.borderRadius = "5px";
 screen.appendChild(box);
 
-const perso = document.createElement('div');
-perso.style.position = "absolute";
-perso.style.bottom = "12px";
-perso.style.left = "25px";
-perso.style.width = "30px";
-perso.style.height = "30px";
-perso.style.backgroundColor = "red";
-perso.style.border = "solid 2px black";
-perso.style.borderRadius = "20px";
-screen.appendChild(perso);
+class Player {
+    constructor(perso) {
+        this.perso = document.createElement('div');
+        this.perso.style.position = "absolute";
+        this.perso.style.bottom = "12px";
+        this.perso.style.left = "25px";
+        this.perso.style.width = "30px";
+        this.perso.style.height = "30px";
+        this.perso.style.backgroundColor = "red";
+        this.perso.style.border = "solid 2px black";
+        this.perso.style.borderRadius = "20px";
+        this.perso.style.transition = "0.3s";
+        screen.appendChild(this.perso);
+        console.log(this.perso);
+    }
+
+    get jumpHeight() {
+        return this.perso.style.bottom;
+    }
+
+    movementRight() {
+        this.perso.style.width = "40px";
+        this.perso.style.left = `${parseInt(this.perso.style.left) + speed}px`;
+    }
+
+    movementLeft() {
+        this.perso.style.width = "40px";
+        this.perso.style.left = `${parseInt(this.perso.style.left) - speed}px`;
+    }
+
+    jump() {
+        this.perso.style.height = '40px';
+        this.perso.style.bottom = `${parseInt(this.perso.style.bottom) + force}px`;
+    }
+
+    resetWidthRight() {
+        this.perso.style.width = "30px";
+        this.perso.style.left = `${parseInt(this.perso.style.left) + speed}px`;
+    }
+
+    resetWidthLeft() {
+        this.perso.style.width = "30px";
+        this.perso.style.left = `${parseInt(this.perso.style.left) - speed}px`;
+    }
+
+    resetHeight() {
+        this.perso.style.height = '30px';
+    }
+
+    inertieAfterJumping() {
+        this.perso.style.bottom = `${parseInt(this.perso.style.bottom) - inertie }px`;
+    }
+}
 
 root.appendChild(screen);
 
+const perso = new Player();
 
 // Make the character move and jump!
+const speed = 30;
+const force = 250;
+const inertie = 50;
+let onAir = false;
+
+PlayerManager();
+
+function PlayerManager() {
+    document.addEventListener('keydown', (e) => {
+        switch (e.keyCode) {
+            case 39:
+                perso.movementRight();
+                break;
+            case 37:
+                perso.movementLeft();
+                break;
+            case 32:
+                if(!onAir) {
+                    perso.jump();
+                    onAir = true;
+                }
+                break;
+        }
+    })
+}
+document.addEventListener('keyup', (e) => {
+    switch (e.keyCode) {
+        case 39:
+            perso.resetWidthRight();
+            break;
+        case 37:
+            perso.resetWidthLeft();
+            break;
+    }
+})
+
+setInterval(() => {
+    if(parseInt(perso.jumpHeight) > 12) {
+        perso.inertieAfterJumping();
+    } else {
+        perso.resetHeight();
+        onAir = false;
+    }
+}, 100)
